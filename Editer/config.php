fe8f961ab37f5387b41d3d3710bc094a2c6cf7f2
fb502902 <brandon0683@hotmail.fr>
2018-02-08 12:32:40 +0100

    edit site

---
+++ Editer/config.php
@@ -0,0 +1,39 @@
+<?php
+$username = "police-portal"; // username
+$password = "your password"; // password of the database
+$hostname = "localhost"; // host of the database
+$namebase = "police"; // name of the database
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Attempt to connect to the database
+try
+ {
+  $bdd = new PDO('mysql:host='.$hostname.';dbname='.$namebase.'', $username, $password);
+ }
+  catch (Exception $e)
+ {
+  // If an error is thrown, display the message
+  die('Erreur : ' . $e->getMessage());
+ }
+ ?>
