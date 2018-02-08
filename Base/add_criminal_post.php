850dc61a813059700a897a4c41f94ecf0463367b
fb502902 <brandon0683@hotmail.fr>
2018-02-08 12:31:54 +0100

    edit repo

---
+++ Base/add_criminal_post.php
@@ -0,0 +1,14 @@
+﻿<?php
+// Calls for the config file
+include( "config.php" );
+
+
+
+// Insert the information
+$req = $bdd->prepare('INSERT INTO lspd (nom, telephone, steam, crime, sanction) VALUES(?, ?, ?, ?, ?)');
+$req->execute(array($_POST['nom'], $_POST['telephone'], $_POST['steam'], $_POST['crime'], $_POST['sanction']));
+
+// Redirect user back to the add criminal page
+header('Location: add_criminal.php');
+
+?>
