d2a1a76d06d18c8c16afa467607d2b13a2298ebf
fb502902 <brandon0683@hotmail.fr>
2018-02-20 19:58:37 +0100

    adit admin link

--- Editer/administration_annonce.php
+++ Editer/administration_annonce.php
@@ -42,7 +42,7 @@ if (isset($_SESSION['id']) and $_SESSION['Admin'] == 1) {
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="administration.php">
+                        <a class="navbar-brand" href="police.php">
                             <img src="https://i.imgur.com/BQoTEoz.png" width=180 height=70/>
                         </a>
                     </div>
