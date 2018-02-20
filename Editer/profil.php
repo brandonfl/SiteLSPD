1997dd0eab0e972ec71881d6b8d2ffa078b96098
fb502902 <brandon0683@hotmail.fr>
2018-02-20 16:13:07 +0100

    profil en cours

--- Editer/profil.php
+++ Editer/profil.php
@@ -16,6 +16,27 @@
             <link href="assets/css/style.css" rel="stylesheet" /> 
             <!-- GOOGLE FONT -->
             <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script>
+
+            $(document).ready(function()
+            {
+                var navItems = $('.admin-menu li > a');
+                var navListItems = $('.admin-menu li');
+                var allWells = $('.admin-content');
+                var allWellsExceptFirst = $('.admin-content:not(:first)');
+                allWellsExceptFirst.hide();
+                navItems.click(function(e)
+                {
+                    e.preventDefault();
+                    navListItems.removeClass('active');
+                    $(this).closest('li').addClass('active');
+                    allWells.hide();
+                    var target = $(this).attr('data-target-id');
+                    $('#' + target).show();
+                });
+            });
+        </script>
         </head>
         <?php
 if (isset($_GET['error'])) {
@@ -167,11 +188,6 @@ if (isset($_SESSION['id'])) {
                             <h4 class="header-line">Profil</h4>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <!-- Advanced Tables -->
-                            <div class="panel panel-default">
-                                <div class="panel-body">
                                         
 
                                     ';
@@ -202,20 +218,170 @@ if (isset($_SESSION['id'])) {
     while ($data = $reponse->fetch()) {
         $pseudo = $data['pseudo'];
         $mail = $data['mail'];
+        $connection = $data['lastConnection'];
+        $admin = $data['Admin'];
     }
     $reponse->closeCursor(); // Complete query
+
+    if($admin == 1){
+        $ad = '<div class="label label-danger">Administrateur</div>';
+    }else{
+        $ad = ' ';
+    }
+
     echo '
-                            pseudo : '.$pseudo.'<br>
-                            mail : '.$mail.'<br>
-                            rang : '.$rang.'<br>
                                             
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+  
+        <div class="row">
+
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked admin-menu" >
+                    <li class="active"><a href="" data-target-id="profile"><i class="glyphicon glyphicon-user"></i> Profile</a></li>
+                    <li><a href="" data-target-id="change-password"><i class="glyphicon glyphicon-lock"></i> Change Password</a></li>
+                    <li><a href="" data-target-id="settings"><i class="glyphicon glyphicon-cog"></i> Settings</a></li>
+                    <li><a href="" data-target-id="logout"><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-9  admin-content" id="profile">
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Name</h3>
+                    </div>
+                    <div class="panel-body">
+                        '.$pseudo.'
+                    </div>
+                </div>
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Email</h3>
+                    </div>
+                    <div class="panel-body">
+                        '.$mail.'
+                    </div>
+                </div>
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Rang</h3>
+                    </div>
+                    <div class="panel-body">
+                         <div class="label label-success">'.$rang.'</div>
+                         '.$ad.'
+                    </div>
+                </div>
+                
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Derniere Connexion</h3>
+
+                    </div>
+                    <div class="panel-body">
+                        '.$connection.'
+                    </div>
+                </div>
+
+            </div>
+   <div class="col-md-9  admin-content" id="settings">
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Notification</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="label label-success">allowed</div>
+                    </div>
+                </div>
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Newsletter</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="badge">Monthly</div>
+                    </div>
+                </div>
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Admin</h3>
+
+                    </div>
+                    <div class="panel-body">
+                         <div class="label label-success">yes</div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-9  admin-content" id="change-password">
+                <form action="/password" method="post">
+
+           
+                    <div class="panel panel-info" style="margin: 1em;">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><label for="new_password" class="control-label panel-title">New Password</label></h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="password" id="new_password" >
+                                </div>
                             </div>
-                            <!--End Advanced Tables -->
+
                         </div>
                     </div>
-                    <!-- /. ROW  -->
+
+             
+                    <div class="panel panel-info" style="margin: 1em;">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><label for="confirm_password" class="control-label panel-title">Confirm password</label></h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" name="password_confirmation" id="confirm_password" >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+           
+                    <div class="panel panel-info border" style="margin: 1em;">
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <div class="pull-left">
+                                    <input type="submit" class="form-control btn btn-primary" name="submit" id="submit">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+
+            <div class="col-md-9  admin-content" id="settings"></div>
+
+            <div class="col-md-9  admin-content" id="logout">
+                <div class="panel panel-info" style="margin: 1em;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Confirm Logout</h3>
+                    </div>
+                    <div class="panel-body">
+                        Do you really want to logout ?  
+                        <a  href="#" class="label label-danger"
+                            onclick="event.preventDefault();
+                                                     document.getElementById(\'logout-form\').submit();">
+                            <span >   Yes   </span>
+                        </a>    
+                        <a href="/account" class="label label-success"> <span >  No   </span></a>
+                    </div>
+                    <form id="logout-form" action="#" method="POST" style="display: none;">
+                    </form>
+
+
+
                 </div>
-                <!-- /. ROW  -->
+
             </div>
             <!-- /. ROW  -->
             <!-- End  Hover Rows  -->
@@ -231,7 +397,7 @@ if (isset($_SESSION['id'])) {
     <div class="row">
         <div class="col-md-12">
                    &copy; 2017 LSPD |
-            <a href="https://www.youtube.com/user/davendrix" target="_blank"  > Coded by : Davendrix</a> &amp; Glen McMahon
+             Coded by :  Glen McMahon
         </div>
     </div>
 </div> </section> <!-- FOOTER SECTION END--> <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  --> <!-- CORE JQUERY  --> <script src="assets/js/jquery-1.10.2.js"></script> <!-- BOOTSTRAP SCRIPTS  --> <script src="assets/js/bootstrap.js"></script> <!-- DATATABLE SCRIPTS  --> <script src="assets/js/dataTables/jquery.dataTables.js"></script> <script src="assets/js/dataTables/dataTables.bootstrap.js"></script> <!-- CUSTOM SCRIPTS  --> <script src="assets/js/custom.js"></script> </body>';
