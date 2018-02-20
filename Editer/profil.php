ed98d60a63b1926793352de85edfd1ad70e2e5ae
fb502902 <brandon0683@hotmail.fr>
2018-02-20 15:11:35 +0100

    add profil

---
+++ Editer/profil.php
@@ -0,0 +1,108 @@
+<html xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <![endif]-->
+            <title>LSPD PANEL</title>
+            <!-- BOOTSTRAP CORE STYLE  -->
+            <link href="assets/css/bootstrap.css" rel="stylesheet" />
+            <!-- FONT AWESOME STYLE  -->
+            <link href="assets/css/font-awesome.css" rel="stylesheet" />
+            <!-- CUSTOM STYLE  -->
+            <link href="assets/css/style.css" rel="stylesheet" /> 
+            <!-- GOOGLE FONT -->
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        </head>
+
+    <head>
+    <link rel="icon" type="image/x-icon" href="https://lspd-fivelife.fr/assets/img/lspdlogo.ico" />
+<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="https://lspd-fivelife.fr/assets/img/lspdlogo.ico" /><![endif]-->
+    </head>
+
+        <body>
+            <div class="navbar navbar-inverse set-radius-zero" >
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php">
+                            <img src="https://i.imgur.com/BQoTEoz.png" width=180 height=70/>
+                        </a>
+                    </div>
+
+                    <div class="right-div">
+                        <a href="login.php" class="btn btn-info pull-right">Se connecter</a>
+                    </div>
+                    <div class="right-div">
+                        <a href="signup.php" class="btn btn-info pull-right">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+            <!-- LOGO HEADER END-->
+            <section class="menu-section">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-md-12">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- MENU SECTION END-->
+            <div class="content-wrapper">
+                <div class="container">
+                    <div class="row pad-botm">
+                        <div class="col-md-12">
+                            <h4 class="header-line">Site LSPD</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- Advanced Tables -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                    <!-- /. ICI  -->
+                    <h3>Recrutement LSPD</h3>
+                                    <center>
+                                    </br>
+                    La LSPD recrute ! Que vous soyez blond, brun, roux, il y a une place pour vous chez nous !
+                    N'hésitez pas à remplir notre <a href="https://docs.google.com/forms/d/e/1FAIpQLSfMFwuD0P086_8tYLPQWr363RLMwvMhiR9Aum2gqMHETJNLqA/viewform"> formulaire de recutement</a> et à contacter l'un des deux capitaines ( @[Capitaine] Alyana Lasticot ou @[Capitaine] Stanislas BARCLAY ) afin de valider la priser en compte de votre candidature.
+                    Nous recherchons des personnes fiables, ayant l'esprit d'équipe et prête à se mettre au service de nos concitoyens. Casier vierge requis. Une expérience dans un domaine de la sécurité est un plus mais pas obligatoire.
+                    </br>
+
+                    <img src="https://cdn.discordapp.com/attachments/399576168770174978/410885422877769733/tousbienvenu.png"/>
+                               </center>
+
+                    <!-- /. ROW  -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. ROW  -->
+            <!-- End  Hover Rows  -->
+        </div>
+        <div class="col-md-6">
+            <!--    Context Classes  -->
+            <!--  end  Context Classes  -->
+        </div>
+    </div>
+    <!-- /. ROW  -->
+</div> </div> <!-- CONTENT-WRAPPER SECTION END--> <section class="footer-section">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+                   &copy; 2017 LSPD |
+            <a href="https://www.youtube.com/user/davendrix" target="_blank"  > Coded by : Davendrix</a> &amp; Glen McMahon
+        </div>
+    </div>
+</div> </section> <!-- FOOTER SECTION END--> <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  --> <!-- CORE JQUERY  --> <script src="assets/js/jquery-1.10.2.js"></script> <!-- BOOTSTRAP SCRIPTS  --> <script src="assets/js/bootstrap.js"></script> <!-- DATATABLE SCRIPTS  --> <script src="assets/js/dataTables/jquery.dataTables.js"></script> <script src="assets/js/dataTables/dataTables.bootstrap.js"></script> <!-- CUSTOM SCRIPTS  --> <script src="assets/js/custom.js"></script> </body></html>
