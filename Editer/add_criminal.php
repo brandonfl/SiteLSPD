d91edd23cab143b7bf228d03f11026a5d415fca6
fb502902 <brandon0683@hotmail.fr>
2018-02-26 10:07:27 +0100

    link

--- Editer/add_criminal.php
+++ Editer/add_criminal.php
@@ -92,24 +92,7 @@
 						<div class="col-md-12">
 							<div class="navbar-collapse collapse ">
 								<ul id="menu-top" class="nav navbar-nav navbar-right">
-									<li>
-                                        <a href="police.php">Home</a>
-                                    </li>
-                                    <li>
-										<a href="add_criminal.php" class="menu-top-active">Ajouter un criminel</a>
-									</li>
-									<li>
-										<a href="bracelet.php">Bracelet</a>
-									</li>
-									<li>
-											<a href="concessionnaire.php">Plaques</a>
-										</li>
-										<li>
-											<a href="trello" target="_blank">Informations Internes</a>
-										</li>
-										<li>
-											<a href="drive" target="_blank">Documents</a>
-										</li>
+								'.$nav.'
 									</ul>
 							</div>
 						</div>
