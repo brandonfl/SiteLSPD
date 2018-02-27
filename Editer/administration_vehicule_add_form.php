28261f35655605e3b49a18444f15c536bb5a3d85
fb502902 <brandon0683@hotmail.fr>
2018-02-27 20:38:03 +0100

    site admin

---
+++ Editer/administration_vehicule_add_form.php
@@ -0,0 +1,17 @@
+﻿<?php
+// Calls for the config file
+include( "config.php" );
+session_start();
+
+if (isset($_SESSION['id']) and  $_SESSION['Admin'] == 1) {
+
+// Insert the information
+$req = $bdd->prepare('INSERT INTO vehicule (plaque,type,assigne) VALUES(?,?,?)');
+$req->execute(array($_POST['plaque'], $_POST['type'], $_POST['pseudo']));
+
+// Redirect user back to the add criminal page
+header('Location: add_criminal.php');
+} else {
+    header("Location: login.php");
+}
+?>
