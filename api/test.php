dcece88b2fa98a92cb120f99689c9ec59d1e833f
fb502902 <brandon0683@hotmail.fr>
2018-03-01 16:28:59 +0100

    debug api

--- api/test.php
+++ api/test.php
@@ -1,12 +1,50 @@
 <?php
-function execInBackground($cmd) {
-            if (substr(php_uname(), 0, 7) == "Windows"){
-           pclose(popen("start /B ". $cmd, "r"));
-        }
-    else {
-                exec($cmd . " > /dev/null &");
-            }
+
+// Method: POST, PUT, GET etc
+// Data: array("param" => "value") ==> index.php?param=value
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
 }
 
-$cmd="php ../app/console EmailS2:EnvoiDirecteMailing ".$contenuMail->getId()." ".$injectionSdp." ".$user->getUsername();
-$this->execInBackground($cmd);
+// Ne pas changer :
+$method = "GET";
+$url = "https://lspd-fivelife.fr/api/add_plaque.php";
+$password = 'bourlay';
+
+
+//A changer
+$plaque = 'test';
+$pseudo = 'Michel';
+$modele = 'twingo';
+
+
+$request = array("plaque" => $plaque,"nom" => $pseudo, "modele" => $modele, "password" => $password);
+
+$this->CallAPI($method, $url, $request);
