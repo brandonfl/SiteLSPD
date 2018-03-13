aba62ed97e0b0679dc2f6a40a33606d426355329
fb502902 <brandon0683@hotmail.fr>
2018-03-13 16:02:14 +0100

    strtoupper plaque

--- Editer/mecanicien_add_controle.php
+++ Editer/mecanicien_add_controle.php
@@ -22,7 +22,7 @@
 
             $nbplaque = 0;
 
-		    $reponse = $bdd->query('SELECT COUNT(*) AS nb FROM plaque WHERE plaque =\''.$_POST['plaque'].'\'');
+		    $reponse = $bdd->query('SELECT COUNT(*) AS nb FROM plaque WHERE plaque =\''.strtoupper($_POST['plaque']).'\'');
 
 
             // Display each entry one by one
